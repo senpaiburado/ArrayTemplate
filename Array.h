ef611a4bbad00abb2d50d2bc5425170a621b41c1
senpaiburado <minerminer354@gmail.com>
2017-04-20 04:10:00 +0400

    Added iterator and some funs

--- Array.h
+++ Array.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <typeinfo>
 
 template <typename T>
 class Array {
@@ -265,11 +266,209 @@ public:
 		}
 	}
 
+	int find_first(const T value) {
+		for (size_t i(0); i < _size; i++)
+		{
+			if (arr[i] == value)
+			{
+				return i;
+			}
+		}
+	}
+
+	int find_last(const T value) {
+		for (size_t i(_size - 1); i >= 0; i--)
+		{
+			if (arr[i] == value)
+			{
+				return i;
+			}
+		}
+	}
+
+	bool contains(const T value) {
+		for (size_t i(0); i < _size; i++)
+		{
+			if (arr[i] == value)
+				return true;
+		}
+		return false;
+	}
+
+	Mode getMode() const {
+		return mode;
+	}
+
+	void clear() {
+		removeAll();
+	}
+
+	Array<T> &operator=(Array<T> array) {
+		//if (mode != array.getMode())
+		//	return NULL;
+		delete[] arr;
+		arr = new T[array.size()];
+		for (size_t i(0); i < array.size(); i++)
+			arr[i] = array[i];
+		_size = array.size();
+		max_size = array.size();
+		return *this;
+	}
+
+	bool operator==(Array<T> array) {
+		if (_size != array.size())
+			return false;
+
+		Array<T> temp1;
+		for (size_t i(0); i < array.size(); i++)
+			temp1.push_back(array[i]);
+		Array<T> temp2;
+		for (size_t i(0); i < _size; i++)
+			temp2.append(arr[i]);
+
+		int count = 0;
+
+		temp1.sortDigits();
+		temp2.sortDigits();
+
+		for (size_t i(0); i < temp1.size(); i++)
+		{
+			if (temp1[i] == temp2[i])
+				count++;
+		}
+
+		if (count == temp1.size())
+			return true;
+		else
+			return false;
+	}
+
+	void sortDigits() {
+			Array<T> temp;
+			Array<T> temp2;
+			int max = 0;
+			int id = 0;
+			int count = 0;
+			for (size_t i(0); i < _size; i++)
+				temp.append(arr[i]);
+
+			while (true)
+			{
+				if (temp.size() == 0)
+					break;
+
+				for (size_t i(0); i < temp.size(); i++)
+				{
+					if (max <= temp[i])
+					{
+						max = temp[i];
+						id = i;
+					}
+				}
+
+				//std::cout << max << std::endl;
+
+				temp2.append(max);
+				temp.remove(id);
+				max = 0;
+				id = 0;
+			}
+			removeAll();
+
+			T *tmp = new T[temp2.size()];
+			for (size_t i(0); i < temp2.size(); i++)
+				tmp[i] = temp2[i];
+
+			//temp2.printDebug();
+			//std::cout << "------------------------------------\n";
+
+			delete[] arr;
+			arr = tmp;
+
+			temp.clear();
+			temp2.clear();
+	}
+
+	void printDebug() {
+		for (size_t i(0); i < _size; i++)
+			std::cout << "Array[" << i << "] = " << arr[i] << std::endl;
+	}
+
+	void print() {
+		for (size_t i(0); i < _size; i++)
+			std::cout << arr[i] << std::endl;
+	}
+
+
+	class Iterator;
+
+	Iterator begin() {
+		return arr;
+	}
+	Iterator end() {
+		return arr + _size;
+	}
+	
 private:
 	int _size;
 	size_t max_size;
 	T *arr;
 	Mode mode;
+	
+
+	class Iterator {
+	public:
+		Iterator(T *first) {
+			current = first;
+		}
+
+		T &operator+(int n) {
+			return *(current + n);
+		}
+		T &operator-(int n) {
+			return *(current - n);
+		}
+		T &operator++(int) {
+			return *current++;
+		}
+		T &operator--(int) {
+			return *current--;
+		}
+		T &operator++() {
+			return *++current;
+		}
+		T &operator--() {
+			return *--current;
+		}
+		Iterator &operator+=(std::ptrdiff_t n) {
+			current = current + n;
+			return *this;
+		}
+		Iterator &operator-=(std::ptrdiff_t n) {
+			current = current - n;
+			return *this;
+		}
+		Iterator &operator-(Iterator &it) {
+			this->current -= *it.current;
+			return *this;
+		}
+		Iterator &operator+(Iterator &it) {
+			this->current += it.current;
+			return *this;
+		}
+
+		bool operator==(const Iterator &it) {
+			return current == it.current;
+		}
+		bool operator!=(const Iterator &it) {
+			return current != it.current;
+		}
+		T &operator*() {
+			return *current;
+		}
+	private:
+		T *current;
+	};
 };
 
 #endif // ARRAY_H_
--- main.cpp
+++ main.cpp
@@ -2,42 +2,29 @@
 
 #include "Array.h"
 #include <conio.h>
+#include <random>
+#include <time.h>
+#include <algorithm>
+
+using namespace std;
 
 int main()
 {
+	srand(time(NULL));
 	Array<int> arr;
+	Array<int> arr2;
+	vector<int> vec;
 
-
-	for (int i(0); i < 15; i++)
+	for (int i(160); i >= 0; i -= 5)
+	{
 		arr.append(i);
+		vec.push_back(i);
+	}
+	for (int i(0); i < 180; i += 5)
+	{
+		arr2.append(i);
+	}
 
-	for (int i(0); i < 15; i++)
-		std::cout << arr[i] << std::endl;
-
-	std::cout << "\n-----------------------------------------------------\n" << std::endl;
-
-	arr.insert(0, 100);
-
-	for (int i(0); i < arr.size(); i++)
-		std::cout << arr[i] << std::endl;
-
-	std::cout << "Length: " << arr.length() << std::endl;
-	std::cout << "Size: " << arr.size() << std::endl;
-	std::cout << "Empty? - " << (arr.isEmpty() ? "Yes!" : "No!") << std::endl;
-	std::cout << "First - " << arr.front() << " || Last - " << arr.back() << std::endl;
-
-	//arr.resize(15);
-
-	std::cout << "\n-----------------------------------------------------\n" << std::endl;
-
-	//arr[5] = 10;
-
-	for (int i(0); i < arr.length(); i++)
-		std::cout << arr[i] << std::endl;
-
-	std::cout << "Length: " << arr.length() << std::endl;
-	std::cout << "Size: " << arr.size() << std::endl;
-	std::cout << "Empty? - " << (arr.isEmpty() ? "Yes!" : "No!") << std::endl;
 	_getch();
 	return 0;
 }
